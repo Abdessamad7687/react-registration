08ec576019035ec68fd529261fa1c0131b5e4a22
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-03-30 11:49:22 +0000

    V1.0

---
+++ src/components/QuestionPage.tsx
@@ -0,0 +1,130 @@
+import { Box, Button, Checkbox, colors, Typography } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import React from "react";
+import CustomInput from "./CustomInput";
+import CustomInput2 from "./CustomInput2";
+
+
+const QuestionPage: React.FC = () => {
+  return (
+    <Grid
+      xs={12}
+      sm={12}
+      md={6}
+      lg={6}
+      xl={6}
+      minHeight={550}
+      sx={{
+        boxShadow: {
+          xs: "",
+          sm: "",
+          md: "15px 2px 5px -5px",
+          lg: "15px 2px 5px -5px",
+          xl: "15px 2px 5px -5px",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: "rgba(0, 24, 57, 0.2)",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100%",
+          borderRadius: {
+            xs: "30px",
+            sm: "30px",
+            md: "30px 0 0 30px",
+            lg: "30px 0 0 30px",
+            xl: "30px 0 0 30px",
+          },
+        }}
+      >
+        <Box width="50%">
+          <Box display="flex" flexDirection="column" alignItems="center">
+            {/* LOGO */}
+            <Box
+              sx={{
+                mt: "60px",
+                width: "150px",
+                height: "50px",
+                bgcolor: "primary.main",
+                borderRadius: "12px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                boxShadow: `0 0 20px ${colors.green[500]}`,
+              }}
+            >
+              <Typography variant="h6" fontWeight="bold" color="white">
+                Register
+              </Typography>
+            </Box>
+
+
+
+          </Box>
+
+          {/* INPUTS */}
+          <br></br>
+          <br></br>
+          <CustomInput
+            label="Industry you are looking for"
+            placeholder=""
+            isIconActive={false}
+          />
+          <br></br>
+          <CustomInput2
+            label="Position you are looking for"
+            placeholder=""
+            isIconActive={false}
+          />
+          <br></br>
+          <CustomInput
+            label="How many connections do you want ?"
+            placeholder=""
+            isIconActive={false}
+          />
+          {/* <CustomInput
+            label="MFA Code"
+            placeholder="Enter your code..."
+            isIconActive={true}
+          /> */}
+          {/* INPUT END */}
+
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            mt={2}
+            width="100%"
+            color="white"
+          >
+            {/* <div style={{ display: "flex" }}>
+              <Checkbox disableRipple sx={{ p: 0, pr: 1 }} />
+              <Typography>Remember me</Typography>
+            </div> */}
+            <a
+              href="#yoyo"
+              style={{
+                color: colors.green[500],
+                textDecoration: "none",
+              }}
+            >
+              {/* Forget password? */}
+            </a>
+          </Box>
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{ mt: 4, boxShadow: `0 0 20px ${colors.green[500]}` }}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Grid>
+  );
+};
+
+export default QuestionPage;
